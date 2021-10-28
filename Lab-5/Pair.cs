ad242c817d240ca52042c75d93c39efab58608a5
аня сергиенко <annsergienko15@gmail.com>
2021-10-28 22:38:14 +0300

    Добавьте файлы проекта.

---
+++ Lab-5/Pair.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PairNumber
+{
+    class Pair
+    {
+        public Pair(int initFirst, int initSecond)
+        {
+            _first = initFirst;
+            _second = initSecond;
+        }
+
+        public double _first;
+        public double _second;
+
+        public double First
+        {
+            get
+            {
+                return _first;
+            }
+            set
+            {
+                _first = value;
+            }
+        }
+        public double Second
+        {
+            get
+            {
+                return _second;
+            }
+            set
+            {
+                _second = value;
+            }
+        }
+        public bool Compare(Pair value)
+        {
+            if (First > value.First || (First == value.First && Second > value.Second)) return true;
+            else return false;
+        }
+        public bool Compare(int firstNumber, int secondNumber)
+        {
+            if (First > firstNumber || (First == firstNumber && Second > secondNumber)) return true;
+            else return false;
+        }
+    }
+}
